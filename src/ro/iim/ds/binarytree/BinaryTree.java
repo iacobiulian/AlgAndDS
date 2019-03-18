c74ca6a37f849ee47387a2e04eafae09c94ac72e
Iacob Iulian Marius <iacob.m96@gmail.com>
2019-03-18 15:24:21 +0200

    Trees

--- src/ro/iim/ds/binarytree/BinaryTree.java
+++ src/ro/iim/ds/binarytree/BinaryTree.java
@@ -1,6 +1,10 @@
 package ro.iim.ds.binarytree;
 
+import ro.iim.algorithms.TestUtils;
+
 import java.nio.file.Paths;
+
+import static ro.iim.algorithms.TestUtils.displayStars;
 
 public class BinaryTree {
     private Node root;
@@ -46,8 +50,88 @@ public class BinaryTree {
         }
     }
 
-    public void delete(int id) {
+    public void delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
 
+        //Find the node and its parent
+        while (current.data != key) {
+            parent = current;
+            if (key < current.data) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+            if (current == null) {
+                System.out.println("Node not found");
+                return;
+            }
+        }
+
+        //1. It is a leaf
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root)
+                root = null;
+            else if (isLeftChild)
+                parent.leftChild = null;
+            else
+                parent.rightChild = null;
+            //2. Only 1 child
+        } else if (current.rightChild == null) { //No right child
+            if (current == root) //Root special case
+                root = current.leftChild;
+            else if (isLeftChild)
+                parent.leftChild = current.leftChild;
+            else
+                parent.rightChild = current.leftChild;
+        } else if (current.leftChild == null) { //No left child
+            if (current == root)
+                root = current.rightChild;
+            else if (isLeftChild)
+                parent.leftChild = current.rightChild;
+            else
+                parent.rightChild = current.rightChild;
+            //3. Has both children
+        } else {
+            Node successor = getSuccessor(current);
+
+            //Connect parent of current to successor instead
+            if(current == root)
+                root = successor;
+            else if(isLeftChild)
+                parent.leftChild = successor;
+            else
+                parent.rightChild = successor;
+
+            //Connect successor to current's left child
+            //Remember successor has no left child to begin with
+            successor.leftChild = current.leftChild;
+        }
+    }
+
+    //https://i.imgur.com/q8YNdSY.png
+    private Node getSuccessor(Node deletedNode) {
+        Node successorparent = deletedNode;
+        Node successor = deletedNode;
+        Node current = deletedNode.rightChild;
+
+        while (current.leftChild != null) {
+            successorparent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+
+        //Remember successor has NO leftChild
+        //https://i.imgur.com/rzWDgOx.png
+        if(successor != deletedNode.rightChild) {
+            successorparent.leftChild = successor.rightChild;
+            successor.rightChild = deletedNode.rightChild;
+        }
+
+        return successor;
     }
 
     private void inOrder(Node root) {
@@ -64,17 +148,53 @@ public class BinaryTree {
         inOrder(this.root);
     }
 
+    public Node minimum() {
+        Node current = root;
+
+        while (current.leftChild != null) {
+            current = current.leftChild;
+        }
+
+        return current;
+    }
+
+    public Node maximum() {
+        Node current = root;
+
+        while (current.rightChild != null) {
+            current = current.rightChild;
+        }
+
+        return current;
+    }
+
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
 
         binaryTree.insert(50);
         binaryTree.insert(25);
         binaryTree.insert(75);
+        binaryTree.insert(55);
+        binaryTree.insert(11);
+        binaryTree.insert(73);
+        binaryTree.insert(52);
+        binaryTree.insert(20);
+        binaryTree.insert(70);
 
         Node found = binaryTree.find(25);
         if (found != null)
             found.displayNode();
         else
             System.out.println("Can not find node");
+
+        binaryTree.traversalInOrder();
+
+        displayStars();
+
+        binaryTree.minimum().displayNode();
+
+        displayStars();
+
+        binaryTree.maximum().displayNode();
     }
 }
